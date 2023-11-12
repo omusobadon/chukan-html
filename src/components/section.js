9de6e0a000e79bae2b1054a09d94cd2003cd6b72
yaffu-kitune <22cc0203@jec.ac.jp>
2023-11-12 15:20:23 +0900

    edit Docs menu ifream

--- src/components/section.js
+++ src/components/section.js
@@ -1,4 +1,49 @@
-//section.js
+class MenuItem {
+  constructor(href, text, controls, isSubItem = false, container) {
+    this.href = href;
+    this.text = text;
+    this.controls = controls;
+    this.isSubItem = isSubItem;
+    this.container = container; // コンテナ ID を保存
+  }
+
+
+  toHTML() {
+    const menuItemClass = this.isSubItem ? "list-group-item list-group-item-action mx-3" : "list-group-item list-group-item-action fs-5";
+    return `
+      <a href="${this.href}" class="${menuItemClass}" data-bs-toggle="collapse" aria-expanded="false" aria-controls="${this.controls}">
+        ${this.text}
+        <i class="fas fa-chevron-down float-end"></i>
+      </a>`;
+  }
+}
+
+class MenuManager {
+  constructor(menuItems) {
+    this.menuItems = menuItems.map(item => {
+      // SubContainer か container のどちらかを使用する
+      const isSubItem = !!item.SubContainer;
+      const container = isSubItem ? item.SubContainer : item.container;
+
+      return new MenuItem(item.href, item.text, item.controls, isSubItem, container);
+    });
+  }
+  addMenuItemsToContainers() {
+    this.menuItems.forEach(item => {
+      const container = document.getElementById(item.container);
+      if (container) {
+        container.innerHTML += item.toHTML();
+      } else {
+        console.error('Container not found for ID:', item.container);
+      }
+    });
+  }
+  
+}
+
+// 既
+
+// 以下は section.js の内容
 const allMenuItems = [
   {
     container: "ms-menu",
@@ -12,25 +57,11 @@ const allMenuItems = [
     text: "Anekiフォント",
     controls: "aneki-ctr",
   },
+  {
+    SubContainer: "ms-Submenu",
+    href: "#ms-Subctr",
+    text: "Sub-フォント",
+    controls: "ms-Subctr",
+  },
   // 他のメニューアイテムも同様に追加
 ];
-
-function createMenuItem(href, text, controls) {
-  return `
-  <a href="${href}" class="list-group-item list-group-item-action fs-5" data-bs-toggle="collapse" aria-expanded="false" aria-controls="${controls}">
-  ${text}
-  <i class="fas fa-chevron-down float-end"></i>
-</a>
-
-    `;
-}
-
-function addMenuItemsToContainers(allMenuItems) {
-  allMenuItems.forEach((item) => {
-    const container = document.getElementById(item.container);
-    if (container) {
-      const menuItemHtml = createMenuItem(item.href, item.text, item.controls);
-      container.innerHTML += menuItemHtml;
-    }
-  });
-}
--- src/components/subsection.js
+++ src/components/subsection.js
@@ -1,24 +1,46 @@
 // script.js
-const ms_font_lis = [
-    { href: './docs/ms-mincho.html', text: 'MS明朝フォント' },
-    // 他のリンクも同様に追加
-  ];
+//MSフォントのリンク
+class FontLink {
+  constructor(href, text, sub = false) {
+    this.href = href;
+    this.text = text;
+    this.sub = sub;
+  }
 
+  static createSubLinks(links) {
+    return links.map(link => new FontLink(link.href, link.text, true));
+  }
+}
+
+//--------------------メニューのアイテム-----------------------
+const ms_font_lis = [
+  new FontLink('ms-mincho.html', 'MS明朝フォント'),
+  // 他のリンクも同様に追加
+];
 
 const aneki_font_lis = [
-    { href: './docs/Zenkurenaido.html', text: 'Zenkurenaidoフォント' },
-    { href: './docs/KleeOne.html', text: 'KleeOneフォント' },
+  new FontLink('KleeOne.html', 'AnekiKleeOneフォント'),
+  new FontLink('Zenkurenaido.html', 'AnekiKurenaidoフォント'),
     // 他のリンクも同様に追加
   ];
+
+//--------------------サブメニューのアイテム--------------------
+// MSサブフォントのリンク
+const rawSubMsFonts = [
+  new FontLink('subfont.html', 'MSゴシックフォント'),
+];
+const sub_ms_font_lis = FontLink.createSubLinks(rawSubMsFonts);
+
+
   
-  function createLink(href, text) {
-    return `<a href="${href}" target="contentFrame" class="list-group-item list-group-item-action mx-3">${text}</a>`;
+  function createLink(href, text, sub) {
+    const mxValue = sub ? 'mx-5' : 'mx-3'; // サブリンクの場合は mx-1、そうでなければ mx-3
+    return `<a href="./docs/${href}" target="contentFrame" class="list-group-item list-group-item-action text-decoration-underline ${mxValue}">${text}</a>`;
   }
   
   function addLinksToContainer(containerId, links) {
     const container = document.getElementById(containerId);
     if (container) {
-      container.innerHTML = links.map(link => createLink(link.href, link.text)).join('');
+      container.innerHTML = links.map(link => createLink(link.href, link.text, link.sub)).join('');
     }
   }
-  
