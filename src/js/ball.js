d9deac032164f7125704b6020dfd07a1bf793bb4
yaffu-kitune <22cc0203@jec.ac.jp>
2023-11-12 21:02:33 +0900

    fixed css bugs

--- src/js/ball.js
+++ src/js/ball.js
@@ -1,41 +1,90 @@
-function createBall(id) {
-  const element = document.getElementById(id);
-  const ballSize = 80; // CSSで設定されたボールのサイズに合わせて調整
-  return {
-    element: element,
-    x: Math.random() * (window.innerWidth - ballSize),
-    y: Math.random() * (window.innerHeight - ballSize),
-    dx: (Math.random() - 0.5) * 4,
-    dy: (Math.random() - 0.5) * 4,
-    rotation: 0,
-  };
-}
+var balls = [];
+var ballRadius = 35;
 
+document.addEventListener("DOMContentLoaded", function() {
+  // DOMContentLoaded内でキャンバスとコンテキストを割り当てます。
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-const balls = [createBall("ball1"), createBall("ball2"), createBall("ball3")];
+  var ballRadius = 35; // 画像の半径
+  var balls = []; // ボールを保持する配列
+  var ballCount = 3; // 生成するボールの数
 
-function update() {
-  balls.forEach((ball) => {
-    const ballWidth = ball.element.offsetWidth;
-    const ballHeight = ball.element.offsetHeight;
+  // ボールの画像をロード
+  var ballImage = new Image();
+  ballImage.src = "./src/img/オムそば丼キャラ切り抜き.png";
 
-    if (ball.x + ball.dx > window.innerWidth - ballWidth || ball.x + ball.dx < 0) {
-      ball.dx = -ball.dx;
-    }
-    if (ball.y + ball.dy > window.innerHeight - ballHeight || ball.y + ball.dy < 0) {
-      ball.dy = -ball.dy;
-    }
+  // ボールオブジェクトを生成する関数
+  function createBall() {
+    return {
+      x: Math.random() * (canvas.width - ballRadius * 2) + ballRadius,
+      y: Math.random() * (canvas.height - ballRadius * 2) + ballRadius,
+      dx: (Math.random() - 0.5) * 4,
+      dy: (Math.random() - 0.5) * 4,
+      rotation: 0,
+    };
+  }
 
-    ball.x += ball.dx;
-    ball.y += ball.dy;
-    ball.rotation += 5;
+  // 複数のボールを初期化
+  for (var i = 0; i < ballCount; i++) {
+    balls.push(createBall());
+  }
 
-    ball.element.style.left = ball.x + "px";
-    ball.element.style.top = ball.y + "px";
-    ball.element.style.transform = `rotate(${ball.rotation}deg)`;
-  });
+  function drawBall(ball) {
+    ctx.save(); // 現在のコンテキスト状態を保存
+    ctx.translate(ball.x, ball.y); // キャンバスの原点をボールの中心に移動
+    ctx.rotate((ball.rotation * Math.PI) / 180); // ボールを回転
+    ctx.drawImage(
+      ballImage,
+      -ballRadius,
+      -ballRadius,
+      ballRadius * 2,
+      ballRadius * 2
+    );
+    ctx.restore(); // コンテキスト状態を復元
+  }
 
-  requestAnimationFrame(update);
-}
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    balls.forEach(function (ball) {
+      drawBall(ball);
 
-update();
+      if (
+        ball.x + ball.dx > canvas.width - ballRadius ||
+        ball.x + ball.dx < ballRadius
+      ) {
+        ball.dx = -ball.dx;
+      }
+      if (
+        ball.y + ball.dy > canvas.height - ballRadius ||
+        ball.y + ball.dy < ballRadius
+      ) {
+        ball.dy = -ball.dy;
+      }
+
+      ball.x += ball.dx;
+      ball.y += ball.dy;
+      ball.rotation += 5; // ここで回転角度を増やす
+    });
+
+    requestAnimationFrame(draw);
+  }
+
+  draw();
+});
+
+// ウィンドウがリサイズされたときにキャンバスのサイズを更新する
+window.addEventListener('resize', function() {
+  if (canvas) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    // サイズ変更後にボールの位置を再計算
+    balls.forEach(function(ball) {
+      ball.x = Math.random() * (canvas.width - ballRadius * 2) + ballRadius;
+      ball.y = Math.random() * (canvas.height - ballRadius * 2) + ballRadius;
+    });
+  }
+});
